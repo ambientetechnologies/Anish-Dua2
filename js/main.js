937eb91543679310b5a4634dd8516eaece0f9948
Nimit-Sachdeva <nimitsachdeva13@gmail.com>
2018-02-21 21:02:47 +0530

    Javascript added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function show() {
+    document.getElementById('left').className +=' col-xs-12 col-md-6 col-lg-6 col-sm-6';
+    document.getElementById('left').style.backgroundSize='cover';
+    document.getElementById('left').style.backgroundImage="url('images/anish.png')";
+    document.getElementById('right').style.display= 'block';
+    document.getElementById("transparent").style.display='none';
+}
